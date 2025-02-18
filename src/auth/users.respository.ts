e668ffd4e889b0b33f612480a6f3321a6bddaf2b
Piyal Chatterjee <piyal.chatterjee@neudesic.com>
2025-02-18 17:13:58 +0530

    #7 feat: add authentication module with signup functionality and user entity

---
+++ src/auth/users.respository.ts
@@ -0,0 +1,30 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Injectable()
+export class UsersRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}

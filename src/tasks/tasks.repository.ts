a183a1cf2f2584901efd0e72cc045ab21613d688
Piyal Chatterjee <piyal.chatterjee@neudesic.com>
2025-02-18 20:08:30 +0530

    #9 feat: add logging to task management operations for better traceability

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -3,16 +3,20 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository');
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager());
   }
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`,
+    );
 
     const query = this.createQueryBuilder('task');
     query.where('task.userId = :userId', { userId: user.id });
@@ -34,7 +38,9 @@ export class TasksRepository extends Repository<Task> {
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
-
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(createTaskDto)}`,
+    );
     const task = this.create({
       title,
       description,
